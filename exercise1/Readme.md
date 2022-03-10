1a259bb6fbd596ada9044604532d58ef5ec2b8e2
Ileenfdz <ileenfdz@gmail.com>
2022-03-10 22:58:40 +0100

    Added an exercise about getting 2 numbers as inputs and showing the sum of them

---
+++ exercise1/Readme.md
@@ -0,0 +1,5 @@
+## Exercise 1
+
+In this exercise I need to create a program that asks the user for a number A and a number B, and displays the result of the sum on the screen.
+
+## Exercise 2
---
+++ exercise1/exercise1.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int num1, num2;
+    cout<<"Give me a number: ";
+    cin>>num1;
+    cout<<"Give me another number: ";
+    cin>>num2;
+    cout<<"This is the sum of the numbers given: "<< num1 + num2 << endl;
+}
