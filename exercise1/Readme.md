53cfc43edaec8fcec997943cd6d754de811a39b4
Ileenfdz <ileenfdz@gmail.com>
2022-03-10 23:06:45 +0100

    Readme for exercise1 done

--- exercise1/Readme.md
+++ exercise1/Readme.md
@@ -3,3 +3,5 @@
 In this exercise I need to create a program that asks the user for a number A and a number B, and displays the result of the sum on the screen.
 
 ## Exercise 2
+
+In this one, I have to write a program that prompts the user for a letter and displays the next character. Note that a char is nothing more than a number with a value represented by a character in the ASCII table following the alphabet.
--- exercise1/exercise1.cc
+++ exercise1/exercise1.cc
@@ -1,12 +1,22 @@
 #include <iostream>
-
 using namespace std;
+int main()
+{
+	int num1;
+	int num2;
+	int suma;
 
-int main(){
-    int num1, num2;
-    cout<<"Give me a number: ";
-    cin>>num1;
-    cout<<"Give me another number: ";
-    cin>>num2;
-    cout<<"This is the sum of the numbers given: "<< num1 + num2 << endl;
+	cout<<"A continuacion,introduzca dos números para sumarlos"<< endl;
+	cout<< "Introduce un número: ";
+	cin>>num1;
+
+	cout<< "Introduzca un segundo número: ";
+	cin>>num2;
+
+	suma=num1+num2;
+	cout<< "La suma de estos es: "<< suma<< endl;
+
+	system ("PAUSE");
+	return 0;
+
 }
